8de8050e582bacd762a877a1d5adfb74bacc71e5
Deepak Singh <singhdk.singh68@gmail.com>
2024-08-26 20:57:10 +0530

    Committing new change by writing test case for negative numbers in String

--- string/src/main/java/com/tdd/string/CalculateString.java
+++ string/src/main/java/com/tdd/string/CalculateString.java
@@ -9,18 +9,29 @@ public class CalculateString {
             if (numbers.isEmpty()) {
                 return 0;
             }
-            String delimiter = "[,\n]"; // Default delimiters
+            String delimiter = "[,|\n]"; // Default delimiters
             if (numbers.startsWith("//")) {
-                int delimiterEndIndex = numbers.indexOf("\n");
+                int delimiterEndIndex = numbers.indexOf('\n');
                 delimiter = numbers.substring(2, delimiterEndIndex);
                 numbers = numbers.substring(delimiterEndIndex + 1);
                 delimiter = delimiter.replace("]", "").replace("[", "\\");
             }
             String[] numArray = numbers.split(delimiter);
             int sum = 0;
+            StringBuilder negatives = new StringBuilder();
             for (String num : numArray) {
-                System.out.println("Number is :-"+num);
-                sum += Integer.parseInt(num);
+                int number = Integer.parseInt(num);
+                if (number < 0) {
+                    if (negatives.length() > 0) {
+                        negatives.append(",");
+                    }
+                    negatives.append(number);
+                } else {
+                    sum += number;
+                }
+            }
+            if (negatives.length() > 0) {
+                throw new IllegalArgumentException("negative numbers not allowed " + negatives);
             }
             return sum;
         }
--- string/src/test/java/com/tdd/string/StringApplicationTests.java
+++ string/src/test/java/com/tdd/string/StringApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest()
 class StringApplicationTests {
@@ -38,5 +39,15 @@ class StringApplicationTests {
 	@Test
 	public void testCustomDelimiter() {
 		assertEquals(6, calculateString.add("//;\n1;2;3"));
+	}
+
+	@Test
+	public void testNegativeNumbers() {
+		try {
+			calculateString.add("1,-2,3,-4");
+			fail("Exception not thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("negative numbers not allowed -2,-4", e.getMessage());
+		}
 	}
 }
